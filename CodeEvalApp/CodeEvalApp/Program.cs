a98c8fd6b12fb3d6129b940187c249673b874193
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:34:08 +0300

    template

---
+++ CodeEvalApp/CodeEvalApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEvalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //open file
+            using (StreamReader reader = new StreamReader(args[0])) {
+                //for each line
+                while (!reader.EndOfStream) {
+                    //read it
+                    string line = reader.ReadLine();
+                    //if not empty
+                    if (null == line) continue;
+                    //TODO: actions here
+
+                    Console.WriteLine(line);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
